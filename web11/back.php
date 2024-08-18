990c859d104dbfe830163010cc5700b29ecac595
Chang Louis <louisvictory0226@gmail.com>
2024-08-19 00:15:29 +0800

    re

--- web11/back.php
+++ web11/back.php
@@ -5,8 +5,8 @@
 // 	'2'=>'123'
 // ];
 
-// echo $Test->count('id');
-
+// echo $Test->save($test);
+// echo 321 ;
 ?>
 
 
@@ -28,19 +28,19 @@
 <iframe name="back" style="display:none;"></iframe>
 	<div id="all">
     	<div id="title">
-        <?=date("m 月 d 號 l")?>| 今日瀏覽: <?=$_SESSION['total'];?> | 累積瀏覽: <?=q("select sum(`total`) as 'total' from `total`")[0]['total'];?> <a href="index.php" style="float:right">回首頁</a></div>
+        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 <a href="index.php" style="float:right">回首頁</a></div>
         <div id="title2">
-        	<a href="index.php" title="健康促進網-回首頁">
+        	<a href="index.php">
 				<img src="./icon/02B01.jpg" alt="">
 			</a>
         </div>
         <div id="mm">
         	<div class="hal" id="lef">
-            	                	    <a class="blo" href="?do=po">分類網誌</a>
-               	                     	    <a class="blo" href="?do=news">最新文章</a>
-               	                     	    <a class="blo" href="?do=pop">人氣文章</a>
-               	                     	    <a class="blo" href="?do=know">講座訊息</a>
-               	                     	    <a class="blo" href="?do=que">問卷調查</a>
+            	                	    <a class="blo" href="?do=acc">帳號管理</a>
+               	                     	    <a class="blo" href="?do=po">分類網誌</a>
+               	                     	    <a class="blo" href="?do=news">最新文章管理</a>
+               	                     	    <a class="blo" href="?do=know">講座管理</a>
+               	                     	    <a class="blo" href="?do=que">問卷管理</a>
                	                 </div>
             <div class="hal" id="main">
             	<div>
@@ -62,11 +62,11 @@
                     <div class="content">
 						<?php
 						$do = $_GET['do']?? 'main';
-						$file = "./frontend/{$do}.php";
+						$file = "./backend/{$do}.php";
 						if(file_exists($file)){
 							include $file;
 						}else{
-							include "./frontend/main.php";
+							include "./backend/main.php";
 						}
 						?>
 					</div>
--- web11/backend/acc.php
+++ web11/backend/acc.php
@@ -1,18 +1,45 @@
+
 <fieldset>
-    <legend>會員註冊</legend>
-    <div style="color:red">*請設定您要註冊的帳號及密碼(最長12個字元)</div>
-    <table>
+    <legend>帳號管理</legend>
+<table class="tab ct">
+    <tr>
+        <td class='clo'>帳號</td>
+        <td class='clo'>密碼</td>
+        <td class='clo'>刪除</td> 
+    </tr>
+    <?php 
+    $users = $User->all();
+    foreach ($users as $user){
+    ?>
+    <tr>
+        <td><?= $user['acc'];?></td>
+        <td><?=str_repeat("*",strlen($user['pw']));?></td>
+        <td><input type="checkbox" name="del" value="<?=$user['id'];?>"></td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
+<div class="ct">
+<button onclick="del()">確定刪除</button>
+<button onclick="clean()">清空選取</button>
+</div>
+
+
+<h2>新增會員</h2>
+<div style="color:red">*請設定您要註冊的帳號及密碼(最常12個字元)</div>
+<table>
         <tr>
             <td class="clo">Step1:登入帳號</td>
             <td><input type="text" name="acc" id="acc"></td>
         </tr>
         <tr>
             <td class="clo">Step2:登入密碼</td>
-            <td><input type="text" name="pw" id="pw"></td>
+            <td><input type="password" name="pw" id="pw"></td>
         </tr>
         <tr>
             <td class="clo">Step3:再次確認密碼</td>
-            <td><input type="text" name="pw2" id="pw2"></td>
+            <td><input type="password" name="pw2" id="pw2"></td>
         </tr>
         <tr>
             <td class="clo">Step4:信箱(忘記密碼時使用)</td>
@@ -21,12 +48,13 @@
         <tr>
             <td>
                 <button onclick="reg()">註冊</button>
-                <button onclick="clear()">清除</button>
+                <button onclick="clean()">清除</button>
             </td>
             <td></td>
         </tr>
     </table>
 </fieldset>
+
 
 <script>
     function reg() {
@@ -56,4 +84,25 @@
         }
 
     }
+
+function del(){
+    let chks=$("input[type='checkbox']:checked")
+    let ids = new Array();
+    console.log('ok')
+    if(chks.length>0){
+        console.log('ok')
+        for(let i = 0; i < chks.length; i++){
+            ids.push(chks[i].value)
+        }
+        $.post("./api/del_user.php", {
+            ids
+        }, ()=>{
+            console.log('ok')
+            location.reload();
+        })
+    }else {
+        alart("沒有帳號要刪除")
+    }
+}
+
 </script>
