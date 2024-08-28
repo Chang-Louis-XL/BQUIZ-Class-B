fa7f61365addb17aae891a108a4ffbde591de397
Chang Louis <louisvictory0226@gmail.com>
2024-08-29 00:41:26 +0800

    re

--- image/back.php
+++ image/back.php
@@ -1,15 +1,5 @@
-﻿<?php include_once "./api/base.php";
-
-// $test = [
-// 	'1'=>'456',
-// 	'2'=>'123'
-// ];
-
-// dd($Test->all())
-
-?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<?php include_once "./api/base.php"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -20,43 +10,30 @@
 	<link href="./css/css.css" rel="stylesheet" type="text/css">
 	<script src="./js/jquery-1.9.1.min.js"></script>
 	<script src="./js/js.js"></script>
-	<style>
-		.alert {
-			background: rgba(51, 51, 51, 0.8);
-			padding: 10px;
-			box-shadow: 2px 2px 10px #999;
-			border-radius: 5px 5px 0 0;
-			color: #FFF;
-			height: 350px;
-			width: 300px;
-			position: fixed;
-			display: none;
-			z-index: 9999;
-			overflow: auto;
-		}
-	</style>
 </head>
 
 <body>
-
+	<div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
+		<pre id="ssaa"></pre>
+	</div>
+	<iframe name="back" style="display:none;"></iframe>
 	<div id="all">
 		<div id="title">
-			<?= date("m 月 d 號 l"); ?> | 今日瀏覽: <?= $_SESSION['total']; ?> | 累積瀏覽:
-			<?= q("select sum(`total`) as 'total' from `total`")[0]['total']; ?>
+			00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36
 			<a href="index.php" style="float:right">回首頁</a>
 		</div>
 		<div id="title2">
-			<a href="index.php" title="健康促進網-回首頁">
+			<a href="index.php">
 				<img src="icon/02B01.jpg" alt="">
 			</a>
 		</div>
 		<div id="mm">
 			<div class="hal" id="lef">
+				<a class="blo" href="?do=acc">帳號管理</a>
 				<a class="blo" href="?do=po">分類網誌</a>
-				<a class="blo" href="?do=news">最新文章</a>
-				<a class="blo" href="?do=pop">人氣文章</a>
-				<a class="blo" href="?do=know">講座訊息</a>
-				<a class="blo" href="?do=que">問卷調查</a>
+				<a class="blo" href="?do=news">最新文章管理</a>
+				<a class="blo" href="?do=know">講座管理</a>
+				<a class="blo" href="?do=que">問卷管理</a>
 			</div>
 			<div class="hal" id="main">
 				<div>
@@ -77,12 +54,11 @@
 					<div class="content">
 						<?php
 						$do = $_GET['do'] ?? 'main';
-						$file = "./frontend/{$do}.php";
-						// file_exists($file)：這個函數會檢查 $file 所指定的文件或目錄是否存在。如果存在，函數會返回 true，如果不存在，則返回 false。
+						$file = "./backend/{$do}.php";
 						if (file_exists($file)) {
 							include $file;
 						} else {
-							include "./frontend/main.php";
+							include "./backend/main.php";
 						}
 
 
