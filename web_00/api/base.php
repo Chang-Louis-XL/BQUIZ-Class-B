0d41271f39e87ec4e35cbf6ea05838fb466e8483
Chang Louis <louisvictory0226@gmail.com>
2024-08-30 00:13:34 +0800

    re

--- web_00/api/base.php
+++ web_00/api/base.php
@@ -1,6 +1,4 @@
 <?php
-session_start();
-
 
 class DB
 {
@@ -11,8 +9,10 @@ class DB
 
     protected $pdo;
 
+
     public function __construct($table)
     {
+
         $this->table = $table;
         $this->pdo = new PDO($this->dsn, 'root', '');
     }
@@ -23,43 +23,38 @@ class DB
         foreach ($array as $key => $value) {
             $tmp[] = "`$key`='$value'";
         }
-
         return $tmp;
     }
 
     public function save($arg)
     {
-        if (isset($arg['id'])) {
+        if (isset($arg)) {
             $tmp = $this->a2s($arg);
-            $sql = "update `$this->table` set" . join(",", $tmp);
-            $sql .= "where `id`={$arg['id']}";
+            $sql = "update `$this->table` " . join(",", $tmp);
+            $sql .= "where `id`='{$arg['id']}'";
         } else {
-            $keys = array_keys($arg);
-            $sql = "insert into `$this->table` (`" . join("`,`", $keys) . "`)values('" . join("','", $arg) . "')";
+            $key = array_keys($arg);
+            $sql = "insert into `$this->table`(`" . join("`,`", $key) . "`)values('" . join("','", $arg) . "')";
         }
-        // dd($arg);
-        // dd(...$arg);
-        // echo $sql;
-        return $this->pdo->exec($sql);
+        return $this->pdo->exec($arg);
     }
 
     public function all(...$arg)
     {
+
         $sql = "select * from `$this->table`";
         if (isset($arg[0])) {
-            if (is_array($arg)) {
-                $tmp = $this->a2s($arg);
-                $sql = "where" . join("&&", $tmp);
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= "where" . join("&&", $tmp);
             } else {
                 $sql .= $arg[0];
             }
         }
-
         if (isset($arg[1])) {
             $sql .= $arg[1];
         }
-        return $this->pdo->query($sql)->fetchall(2);
-
+        return $this->pdo->query($arg)->fetchALL(2);
     }
 
     public function count(...$arg)
@@ -69,71 +64,57 @@ class DB
         if (isset($arg[0])) {
             if (is_array($arg[0])) {
                 $tmp = $this->a2s($arg[0]);
-                $sql .= "where" . join(" && ", $tmp);
+                $sql .= "where" . join("&&", $tmp);
             } else {
                 $sql .= $arg[0];
             }
         }
-
         if (isset($arg[1])) {
             $sql .= $arg[1];
         }
-        // dd($arg);
-        // dd(...$arg);
-        // echo $sql;
-        return $this->pdo->query($sql)->fetchColumn();
-
+        return $this->pdo->query($arg)->fetchColumn();
     }
-
 
     public function find($arg)
     {
+
         $sql = "select * from `$this->table`";
-        if (isset($arg)) {
+        if (is_array($arg)) {
             $tmp = $this->a2s($arg);
-            $sql = "where" . join("&&", $tmp);
+            $sql .= "where" . join("&&", $tmp);
         } else {
             $sql .= "where `id`='$arg'";
         }
-        return $this->pdo->query($sql)->fetch(2);
+        return $this->pdo->query($arg)->fetch(2);
     }
 
     public function del($arg)
     {
+
         $sql = "select * from `$this->table`";
-        if (isset($arg)) {
+        if (is_array($arg)) {
             $tmp = $this->a2s($arg);
-            $sql = "where" . join("&&", $tmp);
+            $sql .= "where" . join("&&", $tmp);
         } else {
             $sql .= "where `id`='$arg'";
         }
         return $this->pdo->exec($sql);
     }
+}
 
-
+function q($sql){
+    $dsn="mysql:host=localhost;charset=utf8;dbname=db11";
+    $pdo= new PDO($dsn,'root','');
+    return $pdo->query($sql)->fetchALL(2);
 }
 
 function to($url){
-    header("location:".$url);
+    header("location:" .$url);
 }
 
-function q($sql)
-{
-    $dsn = "mysql:host=localhost;charset=utf8;dbname=db11";
-    $pdo = new PDO($dsn, 'root', '');
-    return $pdo->query($sql)->fetch(2);
+function dd($arg){
+    echo  "<pre>";
+   print_r($arg);
+    echo "</pre>";
 }
-
-function dd($array)
-{
-    echo "<br>";
-    echo print_r($array);
-    echo "</br>";
-}
-
-$User = new DB("users");
-$Test = new DB("test");
-
-
-
 
